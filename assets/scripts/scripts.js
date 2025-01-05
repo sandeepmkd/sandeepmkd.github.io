91ae3bce077338da2d790f4daa3a5eefa371fcf4
Asus <Sandeep V>
2025-01-04 22:10:18 -0500

    Added files

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,21 @@
+const rotateImages = (offset) => {
+    const slides = document
+        .querySelector("[data-slides]");
+    const activeSlide = slides.querySelector("[data-active]");
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+    if (newIndex < 0) newIndex = slides.children.length - 1;
+    if (newIndex >= slides.children.length) newIndex = 0;
+
+    slides.children[newIndex].dataset.active = true;
+    delete activeSlide.dataset.active;
+}
+
+const buttons = document.querySelectorAll("[data-carousel-button]");
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const offset = button.dataset.carouselButton === 'next' ? 1 : -1;
+        rotateImages(offset);
+    });
+});
+
+window.setInterval(() => {rotateImages(1)}, 5000);
